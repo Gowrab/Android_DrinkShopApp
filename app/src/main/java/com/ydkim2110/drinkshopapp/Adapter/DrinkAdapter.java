ea62679710b20252f176f55bd2969176f836d280
ydkim2110 <ydkim2110@gmail.com>
2018-12-02 16:48:27 +0900

    Add To Cart Dialog

--- app/src/main/java/com/ydkim2110/drinkshopapp/Adapter/DrinkAdapter.java
+++ app/src/main/java/com/ydkim2110/drinkshopapp/Adapter/DrinkAdapter.java
@@ -1,14 +1,22 @@
 package com.ydkim2110.drinkshopapp.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.squareup.picasso.Picasso;
 import com.ydkim2110.drinkshopapp.Interface.IItemClickListener;
 import com.ydkim2110.drinkshopapp.Model.Drink;
@@ -41,10 +49,17 @@ public class DrinkAdapter extends RecyclerView.Adapter<DrinkViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DrinkViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull DrinkViewHolder holder, final int position) {
 
         holder.txt_price.setText(new StringBuilder("$").append(mDrinkList.get(position).Price));
         holder.txt_drink_name.setText(mDrinkList.get(position).Name);
+
+        holder.btn_add_to_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddToCartDialog(position);
+            }
+        });
 
         Picasso.with(mContext)
                 .load(mDrinkList.get(position).Link)
@@ -56,6 +71,50 @@ public class DrinkAdapter extends RecyclerView.Adapter<DrinkViewHolder> {
                 Toast.makeText(mContext, "Clicked!!!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void showAddToCartDialog(int position) {
+        Log.d(TAG, "showAddToCartDialog: called");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.add_to_cart_layout, null);
+
+        // View
+        ImageView img_product_dialog = view.findViewById(R.id.img_cart_product);
+        ElegantNumberButton txt_count = view.findViewById(R.id.txt_count);
+        TextView txt_product_dialog = view.findViewById(R.id.txt_cart_product_name);
+
+        EditText edt_comment = view.findViewById(R.id.edt_comment);
+
+        RadioButton rdi_sizeM = view.findViewById(R.id.rdi_sizeM);
+        RadioButton rdi_sizeL = view.findViewById(R.id.rdi_sizeL);
+
+        RadioButton rdi_sugar_100 = view.findViewById(R.id.rdi_sugar_100);
+        RadioButton rdi_sugar_70 = view.findViewById(R.id.rdi_sugar_70);
+        RadioButton rdi_sugar_50 = view.findViewById(R.id.rdi_sugar_50);
+        RadioButton rdi_sugar_30 = view.findViewById(R.id.rdi_sugar_30);
+        RadioButton rdi_sugar_free = view.findViewById(R.id.rdi_sugar_free);
+
+        RadioButton rdi_ice_100 = view.findViewById(R.id.rdi_ice_100);
+        RadioButton rdi_ice_70 = view.findViewById(R.id.rdi_ice_70);
+        RadioButton rdi_ice_50 = view.findViewById(R.id.rdi_ice_50);
+        RadioButton rdi_ice_30 = view.findViewById(R.id.rdi_ice_30);
+        RadioButton rdi_ice_free = view.findViewById(R.id.rdi_ice_free);
+
+        // set data
+        Picasso.with(mContext)
+                .load(mDrinkList.get(position).Link)
+                .into(img_product_dialog);
+
+        builder.setView(view);
+        builder.setNegativeButton("ADD TO CART", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
     }
 
     @Override
--- app/src/main/java/com/ydkim2110/drinkshopapp/Adapter/DrinkViewHolder.java
+++ app/src/main/java/com/ydkim2110/drinkshopapp/Adapter/DrinkViewHolder.java
@@ -3,6 +3,7 @@ package com.ydkim2110.drinkshopapp.Adapter;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +21,8 @@ public class DrinkViewHolder extends RecyclerView.ViewHolder implements View.OnC
 
     IItemClickListener mIItemClickListener;
 
+    public Button btn_add_to_cart;
+
     public void setIItemClickListener(IItemClickListener IItemClickListener) {
         mIItemClickListener = IItemClickListener;
     }
@@ -30,6 +33,7 @@ public class DrinkViewHolder extends RecyclerView.ViewHolder implements View.OnC
         img_product = itemView.findViewById(R.id.image_product);
         txt_drink_name = itemView.findViewById(R.id.txt_drink_name);
         txt_price = itemView.findViewById(R.id.txt_price);
+        btn_add_to_cart = itemView.findViewById(R.id.btn_add_to_cart);
 
         itemView.setOnClickListener(this);
     }
